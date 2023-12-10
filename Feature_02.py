e59f771972d35bc73524177307b38c1e7ae16670
sb7222 <153311666+sb7222@users.noreply.github.com>
2023-12-10 15:21:17 +0100

    k

---
+++ Feature_02.py
@@ -0,0 +1,3 @@
+def return_odd(x):
+    return [i for i in x if i%2 ==1]
+
--- app.py
+++ app.py
@@ -1,16 +1,20 @@
 import streamlit as st
 from Feature_01 import return_even
-
+from Feature_02 import return_odd
 
 original_list = [i for i in range(10)]
                  
 even_list = return_even(original_list)
+
+odd_list = return_odd(original_list)
 
 st.write("Hooray, we connected everything")
 
 st.write("Hello, this is a self check")
 
 st.write(even_list)
+
+st.write(odd_list)
 
 
 
